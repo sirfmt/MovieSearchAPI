1ef908f42c1dfbb0d302e49797a33ec5de651b02
sirfmt <sirfmt@gmail.com>
2018-06-16 12:12:55 +0200

    Edits to SearchAPI endpoint

---
+++ MvcMovieApp/Data/MvcMovieAppContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovieApp.Models
+{
+    public class MvcMovieAppContext : DbContext
+    {
+        public MvcMovieAppContext (DbContextOptions<MvcMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieApp.Models.Movie> Movie { get; set; }
+    }
+}
--- MvcMovieApp/Startup.cs
+++ MvcMovieApp/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieApp.Models;
 
 namespace MvcMovieApp
 {
@@ -22,6 +24,9 @@ namespace MvcMovieApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieAppContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieAppContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +48,7 @@ namespace MvcMovieApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
